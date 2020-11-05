1f0eea07111d7d3942f146e65bfe6b2300c935c3
jannkasper <jann.kasper@gmail.com>
2020-11-05 04:55:34 +0100

    backend connection

--- src/actions/projectActions.js
+++ src/actions/projectActions.js
@@ -1,13 +1,69 @@
 import * as types from '../constants/ActionTypes'
 import axios from 'axios';
 
-export const addProject = title => ({type: types.ADD_PROJECT, title});
+// export const addProject = title => ({type: types.ADD_PROJECT, title});
+export const addProject = title => {
+    return dispatch => {
+        axios({
+            method: 'post',
+            url: 'https://jkasper-todo-app-api.herokuapp.com/project/add',
+            data: {title: title}
+        })
+            .then(res => {
+                dispatch(readItemsSuccess(types.READ_PROJECTS, res.data.projects));
+            });
+    };
+};
 
-export const addFilter = title => ({type: types.ADD_FILTER, title});
 
-export const addTodo = (projectId, filterId, statusId, text) => ({type: types.ADD_TODO, projectId, filterId, statusId, text});
-export const updateTodo = (id, projectId, filterId, statusId, text) => ({type: types.UPDATE_TODO, id, projectId, filterId, statusId, text});
-export const deleteTodo = (id) => ({type: types.DELETE_TODO, id});
+export const addFilter = title => {
+    return dispatch => {
+        axios({
+            method: 'post',
+            url: 'https://jkasper-todo-app-api.herokuapp.com/filter/add',
+            data: {title: title}
+        })
+            .then(res => {
+                dispatch(readItemsSuccess(types.READ_FILTERS, res.data.filters));
+            });
+    };
+};
+
+export const addTodo = (projectId, filterId, statusId, text) => {
+    return dispatch => {
+        axios({
+            method: 'post',
+            url: 'https://jkasper-todo-app-api.herokuapp.com/todo/add',
+            data: {projectId: projectId, filterId: filterId, statusId: statusId, text: text}
+        })
+            .then(res => {
+                dispatch(readItemsSuccess(types.READ_TODOS, res.data.todos));
+            });
+    };
+};
+
+export const updateTodo = (id, projectId, filterId, statusId, text) => {
+    return dispatch => {
+        axios({
+            method: 'post',
+            url: `https://jkasper-todo-app-api.herokuapp.com/todo/${id}/update`,
+            data: {projectId: projectId, filterId: filterId, statusId: statusId, text: text}
+        })
+            .then(res => {
+                debugger;
+                dispatch(readItemsSuccess(types.READ_TODOS, res.data.todos));
+            });
+    };
+};
+
+export const deleteTodo = (id) => {
+    return dispatch => {
+        axios.post(`https://jkasper-todo-app-api.herokuapp.com/todo/${id}/delete`)
+            .then(res => {
+                dispatch(readItemsSuccess(types.READ_TODOS, res.data.todos));
+            });
+    };
+};
 
 export const readItemsSuccess = (type, items) => ({type, items});
 
